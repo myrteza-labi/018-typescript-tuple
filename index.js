a9d7d55235638f3a35f3a96a0f5ca112902b4f40
myrteza <myrteza@hotmail.fr>
2023-10-07 19:57:07 +0200

    project done

---
+++ index.js
@@ -0,0 +1,7 @@
+var skills;
+skills = ["Programming", 100];
+var color = [255, 0, 0];
+var bgColor;
+var headerColor;
+bgColor = [255, 0, 0];
+headerColor = [255, 0, 0, 0.5];
---
+++ index.ts
@@ -0,0 +1,13 @@
+let skills : [string,number]; 
+
+skills = ["Programming", 100]; 
+
+let color : [number, number, number] = [255,0,0]; 
+
+let bgColor : [number, number, number, number?]; 
+
+let headerColor : [number, number, number, number?]; 
+
+bgColor = [255,0,0]; 
+
+headerColor = [255,0,0,0.5]; 
